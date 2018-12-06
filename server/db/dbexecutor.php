f576e7f7a0d2f82385bd25a31088fcc4b7c51e43
the-radiator <34714232+davide-perez@users.noreply.github.com>
2018-12-06 22:55:42 +0100

    Added transaction handling
    Still to be tested.

--- server/db/dbexecutor.php
+++ server/db/dbexecutor.php
@@ -26,12 +26,51 @@
 
 		public function query($query, $params){
 
-			$result = pg_query_params($this -> connection, $params);
+			return pg_query_params($this -> connection, $query, $params);
 
 		}
 
 
 		public function transaction($queries, $params){
+			$conn = $this -> connection;
+			//carefully log stuff. Do a transaction id enumeration to carefully keep track.
+			if(pg_query($conn, "BEGIN")){
+				echo "Transaction started";
+			}
+			else{
+				throw new DatabaseException("failed to start transaction");
+			}
+
+			$result = $this -> query($queries, $params);
+
+			if($result){
+				echo "Transaction completed. Attempting to commit...";
+				if(pg_query($conn, "COMMIT")){
+					echo "Transaction committed.";
+					return;
+				}
+				else{
+
+					throw new DatabaseException("failed to commit a transaction.");
+			
+				}
+			}
+			else{
+
+				echo "Failed committing. Attempting to roll back...";
+
+				if(pg_query($conn, "ROLLBACK")){
+
+					echo "Transaction successfully rolled back";
+
+				}
+				else{
+
+					throw new DatabaseException("Failed to roll back a transaction.");
+
+				}
+
+			}
 			
 		}
 
