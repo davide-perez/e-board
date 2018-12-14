a2bb737ab3f706019273d6913b0a836571311734
the-radiator <34714232+davide-perez@users.noreply.github.com>
2018-12-15 00:22:26 +0100

    Added truncating description

--- server/queries/homepage.php
+++ server/queries/homepage.php
@@ -32,7 +32,12 @@ require $_SERVER['DOCUMENT_ROOT'] . "/eboard/eboard/server/db/dbconnfactory.php"
 	function create_obj($res){
 		//add option to truncate the description if longer than x chars, adding ... at the end (blablablabla...)
 
-		 return array("title" => $res[0], "description" => $res[1], "image" => $res[2]);
+		$description = $res[1];
+		if(strlen($description) > 50){
+			$description = substr($description, 0, 47) . "...";
+		}
+
+		 return array("title" => $res[0], "description" => $description, "image" => $res[2]);
 
 	}
 
