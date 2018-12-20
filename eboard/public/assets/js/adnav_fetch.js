b6b9b35da86aab55ae7ca9515cac161999455793
the-radiator <34714232+davide-perez@users.noreply.github.com>
2018-12-21 00:32:47 +0100

    Working on adnavigation
    Dinamically fetching the category and the ads

--- eboard/public/assets/js/adnav_fetch.js
+++ eboard/public/assets/js/adnav_fetch.js
@@ -6,9 +6,26 @@ function fetch(){
 				type: 'GET',
 				success: function(data, status){
 					var res = jQuery.parseJSON(data);
-					var content = JSON.stringify(res);
-					console.log("Fetched: " + content);
+					//var content = JSON.stringify(res);
+					console.log("Fetched: " + res);
+					format(res);
 				}
 
 		});
+}
+
+
+
+function setCategory(category) {
+	$(".cat-title").text(category);
+	$(".cat-text").text("Private lectures offerings and requests");
+}
+
+
+function format(content){
+
+	var category = content[0].category;
+	var ads = content[1].ads;
+	setCategory(category);
+
 }
